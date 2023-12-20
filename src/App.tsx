c2a443ce792b747abafdaaaf1656fdcdad59b0d5
Inna-Kulish <norelajest@gmail.com>
2023-12-20 20:56:42 +0200

    mobile menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
 import Navbar from "@/scenes/navbar/Navbar";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { SelectedPage } from "./shared/types";
 
 function App() {
-  const [selectedPage, setSelectedPage] = useState<string>('home');
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      } else {
+setIsTopOfPage(false);
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll)
+})
 
   return (
     <div className='app bg-gray-20'>
-      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
     </div>
   )
 }
