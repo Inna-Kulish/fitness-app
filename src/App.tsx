2fd6da2e1e847db293bd9b0173e0cb8be44fb3e0
Inna-Kulish <norelajest@gmail.com>
2023-12-21 21:02:13 +0200

    contact us section

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { SelectedPage } from './shared/types';
 import Home from './scenes/home/Home';
 import Benefits from './scenes/benefits/Benefits';
 import OurClasses from './scenes/ourclasses/OurClasses';
+import ContactUs from './scenes/contactUs/ContactUs';
+import Footer from './scenes/footer/Footer';
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
@@ -35,6 +37,8 @@ function App() {
       <Home setSelectedPage={setSelectedPage} />
       <Benefits setSelectedPage={setSelectedPage} />
       <OurClasses setSelectedPage={setSelectedPage} />
+      <ContactUs setSelectedPage={setSelectedPage} />
+      <Footer />
     </div>
   );
 }
